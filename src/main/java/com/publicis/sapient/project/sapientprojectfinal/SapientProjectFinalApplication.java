c640f615110c4c5c59c8feca50480157808fd9e1
Abhinav <abhinav.kasaudhan@walmartlabs.com>
2021-04-18 19:55:59 +0530

    first commit

---
+++ src/main/java/com/publicis/sapient/project/sapientprojectfinal/SapientProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package com.publicis.sapient.project.sapientprojectfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapientProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapientProjectFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicis/sapient/project/sapientprojectfinal/SapientProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicis.sapient.project.sapientprojectfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapientProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
